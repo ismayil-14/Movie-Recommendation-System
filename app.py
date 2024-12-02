4ce13f689f47a1fb801802508bca412c8fca3531
ismayil-14 <154823988+ismayil-14@users.noreply.github.com>
2024-12-03 01:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,16 @@ import pickle
 import numpy as np
 import pandas as pd
 
-# Initialize Flask app
 app = Flask(__name__)
-
-# Load the saved model
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Parse input JSON
     data = request.get_json()
     input1 = data.get("input1")
     input2 = data.get("input2")
 
     with open('xgb_model.pkl', 'rb') as file:
         model = pickle.load(file)
-    
     with open("scaler.pkl", 'rb') as file:
         scaler = pickle.load(file)
     with open("merged_movie_data.pkl", 'rb') as file:
@@ -27,10 +21,9 @@ def predict():
         user = pickle.load(file)
 
 
-    # Validate inputs
     if input1 is None or input2 is None:
         return jsonify({"error": "Missing input1 or input2"}), 400
-    # Prepare data for prediction
+        
     filtered_row = df[df['ItemID'] == int(input2)]
     array = filtered_row.to_numpy()
     array = np.insert(array , 0 ,int(input1) , axis = 1  )
@@ -42,11 +35,8 @@ def predict():
     result = scaler.transform(df)
     prediction = model.predict(result)
 
-    # Make prediction
     try:
         prediction =  prediction[0]+1
-
-        
         return jsonify({"prediction": int(prediction)})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
